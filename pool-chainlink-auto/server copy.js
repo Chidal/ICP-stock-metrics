5c54105019222fc1db487a089e2e31e3c9cf84d9
chidal <chidianachebie@gmail.com>
2025-03-24 08:05:50 +0100

    taking out the auto chain link

--- pool-chainlink-auto/server copy.js
+++ pool-chainlink-auto/server copy.js
@@ -1,25 +1,3 @@
 
 app.use(express.json());
 
-
-});
-app.post("/runverify", (req, res) => {
-  const deployCommand =
-    "npx hardhat verify 0xE0a6688486e1a75B1b910B6fAbD0c31D7F09DeD5 --network sepolia";
-
-  exec(deployCommand, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error executing command: ${error.message}`);
-      return res.status(500).send("Error deploying");
-    }
-    console.log(`Deploy output: ${stdout}`);
-    res.status(200).send("Deployment successful");
-  });
-const PORT = process.env.PORT || 4000;
-
-app.listen(
-  PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
-  )
-);

542b3f27810861b0105e99095d8cb625f0094431
chidal <chidianachebie@gmail.com>
2025-03-24 07:59:01 +0100

    taking out the auto chain link

--- pool-chainlink-auto/test/Lock.js
+++ pool-chainlink-auto/test/Lock.js
@@ -14,29 +14,6 @@ describe("Lock", function () {
     const ONE_GWEI = 1_000_000_000;
 
 
-  describe("Withdrawals", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if called too soon", async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture);
-
-        await expect(lock.withdraw()).to.be.revertedWith(
-          "You can't withdraw yet"
-        );
-      });
-
-      it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // We can increase the time in Hardhat Network
-        await time.increaseTo(unlockTime);
-
-        // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-          "You aren't the owner"
-        );
-      });
 
       it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
         const { lock, unlockTime } = await loadFixture(

9570ce67a879d52a6b11b604b6f6ff9d0e1b41c8
chidal <chidianachebie@gmail.com>
2025-03-24 08:05:34 +0100

    taking out the auto chain link

--- pool-chainlink-auto/server copy.js
+++ pool-chainlink-auto/server copy.js
@@ -1,17 +1,7 @@
 
 app.use(express.json());
 
-app.post("/rundeploy", (req, res) => {
-  const deployCommand = "npx hardhat run scripts/deploy.js --network sepolia";
 
-  exec(deployCommand, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error executing command: ${error.message}`);
-      return res.status(500).send("Error deploying");
-    }
-    console.log(`Deploy output: ${stdout}`);
-    res.status(200).send("Deployment successful");
-  });
 });
 app.post("/runverify", (req, res) => {
   const deployCommand =

f07e0c83b6a8a38dc17648ae3383aee913319264
chidal <chidianachebie@gmail.com>
2025-03-24 07:59:17 +0100

    taking out the auto chain link

--- pool-chainlink-auto/test/Lock.js
+++ pool-chainlink-auto/test/Lock.js
@@ -15,17 +15,6 @@ describe("Lock", function () {
 
 
 
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // Transactions are sent using the first signer by default
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).not.to.be.reverted;
-      });
-    });
 
     describe("Events", function () {
       it("Should emit an event on withdrawals", async function () {
